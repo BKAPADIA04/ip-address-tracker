8ba4f0d4dbe678b5aee4796aaaf3ebcc465b7428
Bhavya <bkapadia04@gmail.com>
2023-08-11 01:21:34 +0530

    Base Layout of IP Tracker Page

--- src/components/IPTracker.js
+++ src/components/IPTracker.js
@@ -16,6 +16,44 @@ export default function IPTracker() {
           <input type="text" id="ip-input-text" name="ip-address" placeholder="Search for any IP address or domain" autoComplete='off'></input>
           <input type="submit" value="&#9655;" id="ip-input-submit"></input>
         </form>
+        <div className="data-container">
+          <div className="ip-col">
+            <div className="upper-text ip-Col">
+              IP ADDRESS
+            </div>
+            <div className="lower-text ip-Col">
+              192.212.174.101
+            </div>
+          </div>
+          <div className="vl"></div>
+          <div className="location-col">
+            <div className="upper-text ">
+                LOCATION
+            </div>
+            <div className="lower-text">
+            Brooklyn, NY 10001
+            </div>
+          </div>
+          <div className="vl"></div>
+          <div className="timezone-col">
+            <div className="upper-text">
+                TIMEZONE
+            </div>
+            <div className="lower-text">
+                UTC -05:00
+            </div>
+          </div>
+          <div className="vl"></div>
+          <div className="isp-col">
+            <div className="upper-text">
+              ISP
+            </div>
+            <div className="lower-text">
+            SpaceX
+            Starlink
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
