67f123bb6544ddf34f9d35f0d3e113b857bab510
Bhavya <bkapadia04@gmail.com>
2023-08-15 19:00:00 +0530

    Leaflet JS added.Now Code Refactoring for Background

--- src/App.js
+++ src/App.js
@@ -6,18 +6,23 @@ import {
   getInfoForRequestIP,
 } from "./components/Geolocation";
 import React, { useState, useEffect } from "react";
+import Leaflet from "./components/Leaflet";
+import LoadingBar from 'react-top-loading-bar'
 
 // import IPTracker from './components/IPTracker';
 
 function App() {
   document.title = 'IP ADDRESS TRACKER';
+  const [progress,setProgress] = useState(0);
   const [ipAddress, setIpAddress] = useState();
   const [city, setCity] = useState();
   const [region, setRegion] = useState();
   const [country, setCountry] = useState();
   const [timezone, setTimezone] = useState();
   const [isp, setIsp] = useState();
-  const [latlong, setLatLong] = useState([0, 0]);
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+  // const [latlong, setLatLong] = useState([0, 0]);
 
   const ipSubmitHandler = async (ipAddress) => {
     await getInfoByAPI({ ipAddress: ipAddress, useCurrentRequest: false });
@@ -25,11 +30,13 @@ function App() {
 
   let geoInfo;
   const getInfoByAPI = async ({ ipAddress, useCurrentRequest }) => {
+    setProgress(50);
     if (useCurrentRequest) {
       geoInfo = await getInfoForCurrentIP();
     } else {
       geoInfo = await getInfoForRequestIP(ipAddress);
     }
+    setProgress(100);
     console.log(geoInfo);
     if (geoInfo.isOK) {
       setIpAddress(geoInfo.data.ip);
@@ -38,7 +45,10 @@ function App() {
       setCountry(geoInfo.data.location.country);
       setTimezone(geoInfo.data.location.timezone);
       setIsp(geoInfo.data.isp);
-      setLatLong([geoInfo.data.location.lat, geoInfo.data.location.lng]);
+      setLat(geoInfo.data.location.lat);
+      setLng(geoInfo.data.location.lng);
+      // setLatLong([geoInfo.data.location.lat,geoInfo.data.location.lng]);
+      // console.log(latlong);
     } else {
       console.log("An error has occurred ", geoInfo.error);
     }
@@ -48,10 +58,16 @@ function App() {
     getInfoByAPI({ useRequestAddress: true, ipAddress: "" });
   };
 
-  useEffect(getInfoDefault, []);
+  useEffect(getInfoDefault,[]);
 
+  
   return (
     <div>
+    <LoadingBar
+    color='#f11946'
+    progress={progress}
+    height={4}
+    />
       <IPInput onSubmitToApp={ipSubmitHandler} />
       <IPResult
         ipAddress={ipAddress}
@@ -60,7 +76,9 @@ function App() {
         country={country}
         time={timezone}
         isProvider={isp}
+        progress = {setProgress}
       />
+      <Leaflet lat = {lat} lng = {lng}/>
     </div>
   );
 }
---
+++ src/components/Leaflet.js
@@ -0,0 +1,44 @@
+import { MapContainer } from "react-leaflet/MapContainer";
+import { TileLayer } from "react-leaflet/TileLayer";
+import { Marker } from "react-leaflet/Marker";
+import { useMap } from "react-leaflet";
+import React from "react";
+import './Leaflet.css';
+import "leaflet/dist/leaflet.css";
+import L from 'leaflet';
+
+
+const svgIcon = L.divIcon({
+    html: `<svg fill="#ff0000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 395.71 395.71" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M197.849,0C122.131,0,60.531,61.609,60.531,137.329c0,72.887,124.591,243.177,129.896,250.388l4.951,6.738 c0.579,0.792,1.501,1.255,2.471,1.255c0.985,0,1.901-0.463,2.486-1.255l4.948-6.738c5.308-7.211,129.896-177.501,129.896-250.388 C335.179,61.609,273.569,0,197.849,0z M197.849,88.138c27.13,0,49.191,22.062,49.191,49.191c0,27.115-22.062,49.191-49.191,49.191 c-27.114,0-49.191-22.076-49.191-49.191C148.658,110.2,170.734,88.138,197.849,88.138z"></path> </g> </g></svg>`,
+    className: "svg-icon",
+    iconSize: [40, 40],
+    iconAnchor: [20, 40]
+  });
+
+export default function Leaflet(props) {
+  const position = [+props.lat, +props.lng];
+  return (
+    <div className="mapContainer">
+      <MapContainer
+        style={{ height: "100vh", width: "100vw" }}
+        center={position}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position} icon={svgIcon}></Marker>
+        <LeafletMapController targetCenter={position} />
+      </MapContainer>
+    </div>
+  );
+}
+
+function LeafletMapController(props) {
+  const map = useMap();
+  map.removeControl(map.zoomControl);
+  map.flyTo(props.targetCenter);
+  return null;
+}
